ac4509df396093705b8960db65b87afe2e898108
LanceStasinski <lance.stasinski@gmail.com>
2022-03-16 11:51:43 -0400

    feat: Add function that gets optimal num components

---
+++ revised pls/getNComps.R
@@ -0,0 +1,25 @@
+################################################################################
+# Calculate optimal number of components from downsampling plsda model objects
+################################################################################
+getNComps = function(directory, ncomp) {
+  models = list.files(path = directory)
+  accuracies = matrix(nrow = ncomp)
+  
+  for(i in 1:length(models)) {
+    model = readRDS(paste(directory, models[i], sep = "/"))
+    accuracies = cbind(accuracies, as.matrix(model$results$Accuracy))
+  }
+  
+  accuracies = accuracies[,-1]
+  meanAcc = as.matrix(rowMeans(accuracies))
+  sdAcc = as.matrix(rowSds(accuracies))
+  lowerSd = meanAcc - sdAcc
+  #get accuracies less than the standard deviation of the highest mean accuracy
+  lowerMeans = meanAcc[meanAcc < lowerSd[which.max(meanAcc)]]
+  optimalComp = length(lowerMeans) + 1 #first component with an average 
+  # accuracy within 1 sd of the component with the highest mean accuracy
+  
+  return(optimalComp)
+}
+
+saveRDS(getNComps, "revised pls/functions/getNComps.rds")
--- revised pls/index.R
+++ revised pls/index.R
@@ -6,8 +6,32 @@
 
 library(parallel)
 
+#functions
 runPlsda = readRDS("revised pls/functions/runPlsda.rds")
-spectra = readRDS('spectra/lichen_spectra.rds')
+getNComps = readRDS("revised pls/functions/getNComps.rds")
+
+#spectra
+spectra = readRDS("spectra/lichen_spectra.rds")
+
+# set the directories you would like the downsampled and upsampled model 
+# objects to be saved to. The upsampled models will be what you draw your
+# conclusions from. The downsampled models will be used to determine the optimal
+# number of components to use in the upsampling procedure
+downSamplingDirectory = "revised pls/data/downsampling"
+upSamplingDirectory = "revised pls/data/upsampling"
+
+# choose class you are trying to classify - scientificName, family, order, or class
+className = 'scientificName'
+
+# include the effect of age in the model?
+includeAge = FALSE
+
+# number of components to start with. I'm arbitrarily choosing 40, but you can
+# change this
+ncomps = 4 
+
+# choose the number of iterations you want to run plsda for (I suggest 100)
+iterations = seq(1, 4)
 
 ################################################################################
 # Classification - parallelized
@@ -15,14 +39,20 @@ spectra = readRDS('spectra/lichen_spectra.rds')
 
 # choose number of cores - this code chooses half the cores available on your
 # machine
-numCores = detectCores()
-cluster = makeCluster(names = floor(numCores/2))
-
-# choose the number of iterations you want (I suggest 100)
-iterations = seq(1, 4)
+numCores = floor(detectCores()/2)
+cluster = makeCluster(numCores)
 
 # this function will run PLSDA and save model objects into the specified directory
 # the console will log a list of NULL values, don't worry about this
 parLapply(cl = cluster, iterations, runPlsda, spectra = spectra,
-          className = "scientificName", ncomp = 3, resampling = 'down',
-          include_age = F, directory = "revised pls/data")
+          className = className, ncomp = ncomps, resampling = "down",
+          include_age = includeAge, directory = downSamplingDirectory)
+
+# get optimal number of components from downsampled model objects
+optimalNumComps = getNComps(downSamplingDirectory, ncomps)
+
+# run plsda with upsampling with optimal number of components
+parLapply(cl = cluster, iterations, runPlsda, spectra = spectra,
+          className = className, ncomp = optimalNumComps, resampling = "up",
+          include_age = includeAge, directory = upSamplingDirectory)
+
