31803d0547b925d31c9d2904ffb841f7d84bb517
LanceStasinski <lance.stasinski@gmail.com>
2022-03-28 10:16:10 -0400

    refactor: Compute ncomps using 2 standard deviations

--- revised pls/getNComps.R
+++ revised pls/getNComps.R
@@ -8,17 +8,18 @@ getNComps = function(directory, ncomp) {
   
   for(i in 1:length(models)) {
     model = readRDS(paste(directory, models[i], sep = "/"))
-    accuracies = cbind(accuracies, as.matrix(model$results$Accuracy))
+    accuracies = cbind(accuracies, as.matrix(model))
   }
   
   accuracies = accuracies[,-1]
   meanAcc = as.matrix(rowMeans(accuracies))
   sdAcc = as.matrix(rowSds(accuracies))
-  lowerSd = meanAcc - sdAcc
-  #get accuracies less than the standard deviation of the highest mean accuracy
+  lowerSd = meanAcc - (2 * sdAcc)
+  meanAcc = meanAcc[1:which.max(meanAcc),]
+  #get accuracies less than two standard deviations of the highest mean accuracy
   lowerMeans = meanAcc[meanAcc < lowerSd[which.max(meanAcc)]]
   optimalComp = length(lowerMeans) + 1 #first component with an average 
-  # accuracy within 1 sd of the component with the highest mean accuracy
+  # accuracy within 2 sd of the component with the highest mean accuracy
   
   return(optimalComp)
 }
